4721e8d6624472b0fb2dc105f1dbe34d7069e8d9
furbo1 <al_razvan@yahoo.com>
2021-04-28 10:06:54 +0200

    Added viewmodels!

---
+++ LMS.Core/Entities/ViewModels/CourseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Core.Entities.ViewModels
+{
+    public class CourseViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        ICollection<Module> Modules { get; set; }
+    }
+}
