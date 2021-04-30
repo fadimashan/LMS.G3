8b169b0f6f1d85b3a49d4fec77e86f972dc37c8d
furbo1 <al_razvan@yahoo.com>
2021-04-30 13:20:09 +0200

    Added Mapper!

---
+++ LMS.Data/Data/MapperProfile.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using LMS.Core.Entities;
+using LMS.Core.Entities.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Data.Data
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+
+            CreateMap<ApplicationUser, ApplicationUserViewModel>()
+            .ForMember(dest => dest.FullName,
+            from => from.MapFrom(m => m.FullName))
+            .ForMember(dest => dest.Course.Id,
+            from => from.MapFrom(m => m.CourseId))
+            .ForMember(dest => dest.Course.Title,
+            from => from.MapFrom(m => m.CourseId))
+            .ForMember(dest => dest.Course.Description,
+            from => from.MapFrom(m => m.CourseId))
+            .ForMember(dest => dest.Course.StartDate,
+            from => from.MapFrom(m => m.CourseId))
+            .ForMember(dest => dest.Course.EndDate,
+            from => from.MapFrom(m => m.CourseId))
+             .ForMember(dest => dest.Email,
+            from => from.MapFrom(m => m.Email))
+            ;
+
+        }
+    }
+}
