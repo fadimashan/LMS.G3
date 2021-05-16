80b43fbbff876db67204ccc03211cb954f5d8eef
furbo1 <al_razvan@yahoo.com>
2021-05-16 20:37:13 +0200

    Fixed search students & Activity deadline show/hide.

--- LMS.Web/wwwroot/js/site.js
+++ LMS.Web/wwwroot/js/site.js
@@ -62,50 +62,7 @@ select1.addEventListener('change', () => {
 
 let select2 = document.getElementById('assignment2');
 
-let selectActivity = document.getElementById('act');
-let selectActivity2 = document.getElementById('act2');
-selectActivity.addEventListener('change', () => {
-
-    if (selectActivity.value === "B" || selectActivity.value === "C" || selectActivity.value === "D") {
-        selectActivity2.style.display = "none";
-    } else {
-        selectActivity2.style.display = "block";
-    }
-});
 
 
-// From Upload_File_Refresh branch 
-/*
-let table = document.getElementById("test22");
-let table2 = table.cloneNode(table);
-let uploadBtn = document.getElementById("uploadBtn");
 
-uploadBtn.addEventListener("click", () => {
-    console.log("click");
-    let courseId = uploadBtn.getAttribute("data-target");
-    let updatedView = document.querySelectorAll("TR");
-    updatedView.forEach(row => {
-        if (row.classList.contains("collapse")) {
-            row.classList.remove("collapse");
-        }
-    })
-});
 
-var target = document.querySelector('#drop2');
-
-// create an observer instance
-var observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-        console.log(mutation.type);
-    });
-});
-
-// configuration of the observer:
-var config = { attributes: true, childList: true, characterData: true }
-
-// pass in the target node, as well as the observer options
-observer.observe(target, config);
-
-// later, you can stop observing
-observer.disconnect();
-*/
